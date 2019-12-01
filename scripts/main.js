a4fe1851f7d770de2451f9b28317e0c75f755cab
Lindsey Hiltner <lindsey.hiltner@gmail.com>
2019-11-30 20:28:10 -0800

    Add social links; allow users to copy email to clipboard

--- scripts/main.js
+++ scripts/main.js
@@ -4,4 +4,17 @@ highlightActive = (e) => {
   $(e.target).addClass('active');
 };
 
+// source: https://stackoverflow.com/questions/22581345/click-button-copy-to-clipboard-using-jquery#30905277
+copyEmailToClipboard = () => {
+  const email = 'lindsey.hiltner@gmail.com';
+  const aux = document.createElement('input');
+  aux.setAttribute('value', email);
+  document.body.appendChild(aux);
+
+  aux.select();
+  document.execCommand('copy');
+  document.removeChild(aux);
+}
+
 $('.nav-link').on('click', highlightActive);
+$('#email').on('click', copyEmailToClipboard);
