b45bcf4cc672b09b89628a65ce3f9f2703837487
Lindsey Hiltner <lindsey.hiltner@gmail.com>
2020-07-18 19:17:15 -0700

    Remove email from script

--- scripts/main.js
+++ scripts/main.js
@@ -31,18 +31,6 @@ highlightActive = (element) => {
   $(element).addClass('active');
 };
 
-// source: https://stackoverflow.com/questions/22581345/click-button-copy-to-clipboard-using-jquery#30905277
-copyEmailToClipboard = () => {
-  const email = 'lindsey.hiltner@gmail.com';
-  const aux = document.createElement('input');
-  aux.setAttribute('value', email);
-  document.body.appendChild(aux);
-
-  aux.select();
-  document.execCommand('copy');
-  document.removeChild(aux);
-}
-
 $('.nav-link').on('click', (e) => highlightActive(e.target));
 $('#email').on('click', copyEmailToClipboard);
 $(window).scroll(highlightOnScroll);
